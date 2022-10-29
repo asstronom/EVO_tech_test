c798a12f798ee0f9afc78bf955f54fd900baf40b
@pudgebooster <ukraniandollar@gmail.com>
2022-10-29 20:23:46 +0300

    feature: add domain

---
+++ pkg/domain/domain.go
@@ -0,0 +1,27 @@
+package domain
+
+import "time"
+
+type Transaction struct {
+	ID                int
+	RequestID         int
+	TerminalID        int
+	PartnerObjectID   int
+	AmountTotal       float64
+	AmountOriginal    float64
+	CommisionPs       float64
+	CommisionClient   float64
+	CommisionProvider float64
+	DateInput         time.Time
+	DatePost          time.Time
+	Status            string
+	PaymentType       string
+	PaymentNumber     string
+	ServiceID         string
+	Service           string
+	PayeeID           int
+	PayeeName         string
+	PayeeBankMfo      int
+	PayeeBankAccount  int
+	PaymentNarrative  string
+}
