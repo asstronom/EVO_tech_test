91bb54b897d66f3fde94abf7ddf498b0e1989112
@pudgebooster <ukraniandollar@gmail.com>
2022-10-29 14:14:14 +0300

    feature: add first version of a db

---
+++ pkg/migrate/sql/000001_init_schema.down.sql
@@ -0,0 +1,12 @@
+DROP TABLE IF EXISTS PaymentTypes;
+
+Drop TABLE IF EXISTS ServiceTypes;
+
+Drop TABLE IF EXISTS StatusTypes;
+
+Drop TABLE IF EXISTS PayeeNames;
+
+Drop TABLE IF EXISTS PaymentNarratives;
+
+Drop TABLE IF EXISTS Transcations;
+
