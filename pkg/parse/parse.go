05743ed1dc2f246947b31ac3cc72b28246abd4d4
asstronom <ukraniandollar@gmail.com>
2022-12-10 23:24:06 +0200

    feat: add new struct

--- pkg/parse/parse.go
+++ pkg/parse/parse.go
@@ -13,6 +13,39 @@ import (
 
 const layout = "2006-01-02 15:04:05"
 
+type DateTime struct {
+	time.Time
+}
+
+func (date *DateTime) UnmarshalCSV(csv string) (err error) {
+	date.Time, err = time.Parse(layout, csv)
+	return err
+}
+
+type transactionParse struct {
+	ID                int
+	RequestID         int
+	TerminalID        int
+	PartnerObjectID   int
+	AmountTotal       float64
+	AmountOriginal    float64
+	CommisionPs       float64
+	CommisionClient   float64
+	CommisionProvider float64
+	DateInput         time.Time
+	DatePost          time.Time
+	Status            string
+	PaymentType       string
+	PaymentNumber     string
+	ServiceID         int
+	Service           string
+	PayeeID           int
+	PayeeName         string
+	PayeeBankMfo      int
+	PayeeBankAccount  string
+	PaymentNarrative  string
+}
+
 // parses date string, format is 'YYYY-MM-DD HH-MM-SS'
 func ParseDate(datestr string) (time.Time, error) {
 	t, err := time.Parse(layout, datestr)
